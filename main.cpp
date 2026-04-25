b96805e75b684a68bcfecd55f4dca682539bd4f2
Theshakyhorse <amaximus.sierra@gmail.com>
2026-04-24 20:46:26 -0700

    3rd

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 const int Size = 2;
 
 int main() {
+    srand(time(0));
     //decalres a deque for Car
     deque<Car> line;
 
@@ -17,9 +18,29 @@ int main() {
     }
 
     //outputs data
+    cout << "Initial queue:" << endl;
     for (int i = 0; i < line.size(); i++) {
         if (!line.empty()) {
             line[i].print();
+        }
+    }
+    cout << endl;
+
+    for (int i = line.size(); i > 0; i--){
+        cout << "Time: " << (line.size() - i)+1 << endl;
+        int r = rand()%100;
+        if (!line.empty()) {
+            if (r < 45) {
+                Car c = Car();
+                line.push_back(c);
+                c.print();
+                cout << " joins the line" << endl;
+            }
+            else {
+                line[0].print();
+                line.pop_front();
+                cout << " pays its toll and leaves" << endl;
+            }
         }
     }
 
