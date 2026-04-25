343cda5e6049ab97d2f923bd0e20bc4f503069ea
Theshakyhorse <amaximus.sierra@gmail.com>
2026-04-24 20:25:42 -0700

    2nd

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 //COMSC 210 | Lab 32 | Alexander Sierra
 #include <iostream>
+#include "Car.h"
+#include <deque>
 using namespace std;
 
+const int Size = 2;
+
 int main() {
-    
+
+    deque<Car> line;
+
+    for (int i = 0; i < Size; i++){
+        Car c = Car();
+        line.push_back(c);
+    }
+
+    for (int i = line.size(); i > 0; i++) {
+        
+        line.pop_front();
+    }
+
     return 0;
 }
