11bfa90bfcd16600488b74790d8e7a2eeee25fdf
Theshakyhorse <amaximus.sierra@gmail.com>
2026-04-26 17:58:25 -0700

    Milestone 2

--- main.cpp
+++ main.cpp
@@ -5,63 +5,65 @@
 #include <iomanip>
 using namespace std;
 
-const int Size = 2, W = 15;
+const int Size = 2, W = 15, LANES = 4;
 
+//I commented out a lot of stuff becuase it breaks
+//and I can't check if the array of deques works
 int main() {
     srand(time(0));
-    //decalres a deque for Car
-    deque<Car> line;
+    //declaration of array of deques
+    deque<Car> line[LANES];
 
-    //fills the deque with data
-    for (int i = 0; i < Size; i++){
+    //fills with data
+    for (int i = 0; i < LANES; i++){
         Car c = Car();
-        line.push_back(c);
+        line[i].push_back(c);
     }
 
     //outputs data
     cout << "Initial queue:" << endl;
-    for (int i = 0; i < line.size(); i++) {
-        if (!line.empty()) {
+    for (int i = 0; i < LANES; i++) {
+        if (!line[i].empty()) {
             cout << setw(W);
-            line[i].print();
+            line[i][0].print();
             cout << endl;
         }
     }
     
     int i = 1;
     //performs probability based actions until deque is empty
-    while (!line.empty()){
-        cout << "Time: " << i << " Operation: ";
-        int r = rand()%100;
-        if (!line.empty()) {
-            if (r < 45) {
-                Car c = Car();
-                line.push_back(c);
-                cout << "joined lane: ";
-                c.print();
-            }
-            else {
-                cout << "Car paid: ";
-                line[0].print();
-                line.pop_front();
-            }
-        }
+    //while (!line.empty()){
+        //cout << "Time: " << i << " Operation: ";
+        //int r = rand()%100;
+        //if (!line.empty()) {
+            //if (r < 45) {
+                //Car c = Car();
+                //line.push_back(c);
+                //cout << "joined lane: ";
+                //c.print();
+            //}
+            //else {
+                //cout << "Car paid: ";
+                //line[0].print();
+                //line.pop_front();
+            //}
+        //}
         //outputs current data in deque
-        cout << endl << "Queue:" << endl;
-        if (!line.empty()) {
-            for (int i = 0; i < line.size(); i++) {
-                if (!line.empty()) {
-                    cout << setw(W);
-                    line[i].print();
-                    cout << endl;
-                }
-            }
-        }
-        else {
-            cout << setw(W) << "Empty" << endl;
-        }
-        i++;
-    }
+        //cout << endl << "Queue:" << endl;
+        //if (!line.empty()) {
+            //for (int i = 0; i < line.size(); i++) {
+                //if (!line.empty()) {
+                    //cout << setw(W);
+                    //line[i].print();
+                    //cout << endl;
+                //}
+            //}
+        //}
+        //else {
+            //cout << setw(W) << "Empty" << endl;
+        //}
+        //i++;
+    //}
 
     return 0;
 }
