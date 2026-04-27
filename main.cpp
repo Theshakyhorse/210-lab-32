6338d7ef55cb5db7e6596b9ef06ed029c80e6fab
Theshakyhorse <amaximus.sierra@gmail.com>
2026-04-26 18:24:19 -0700

    2nd

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <iomanip>
 using namespace std;
 
-const int Size = 2, W = 15, LANES = 4;
+const int Size = 2, W = 15, LANES = 4, runs = 20, prob = 50;
 
 int main() {
     srand(time(0));
@@ -32,13 +32,13 @@ int main() {
         }
     }
     
-    int i = 1;
-    //performs probability based actions until deque is empty
-    //while (!line.empty()){
-        //cout << "Time: " << i << " Operation: ";
-        //int r = rand()%100;
+    //performs probability based actions for 20 time periods
+    for (int t = 0; t < 20; t++){
+        cout << "Time: " << t+1 << " Operation: ";
+        
+        int r = rand()%100 +1;
         //if (!line.empty()) {
-            //if (r < 45) {
+            //if (r <= prob) {
                 //Car c = Car();
                 //line.push_back(c);
                 //cout << "joined lane: ";
@@ -50,22 +50,20 @@ int main() {
                 //line.pop_front();
             //}
         //}
-        //outputs current data in deque
+        //outputs current data
         //cout << endl << "Queue:" << endl;
-        //if (!line.empty()) {
-            //for (int i = 0; i < line.size(); i++) {
-                //if (!line.empty()) {
-                    //cout << setw(W);
-                    //line[i].print();
-                    //cout << endl;
-                //}
-            //}
-        //}
-        //else {
-            //cout << setw(W) << "Empty" << endl;
-        //}
-        //i++;
-    //}
+        for (int i = 0; i < LANES; i++) {
+            cout << "Lane " << i+1 << ":" << endl;
+            for (int j = 0; j < Size; j++) {
+                if (!line[i].empty()) {
+                    cout << setw(W);
+                    line[i][j].print();
+                    cout << endl;
+                }
+                else {cout << setw(W) << "empty" << endl;}
+            }
+        }
+    }
 
     return 0;
 }
